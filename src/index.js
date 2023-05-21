eb6f7e0089ba8eb2302366e4a077d7ba916e6cec
nguyentran <nt85272@gmail.com>
2023-05-21 12:27:28 +0700

    Road to PWA

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import store, {persistor} from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Loading from "../src/components/Loading"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,4 +22,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+serviceWorkerRegistration.unregister();
 reportWebVitals();
--- src/page/API.js
+++ src/page/API.js
@@ -2,13 +2,7 @@
 import React, { useState, useEffect } from 'react';
 
 import img from '../constants';
-
-
-
 export default function API() {
-  
-  
-
   return (
     <div>
       <img style={{objectFit: "contain", height: '100%', width: '100%'}}src={img.api}  />
